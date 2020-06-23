8c285c5334d5971dc8daeabf98005bb14550f1f3
Waqar Ahmed <57212390+WaqarAhmedk@users.noreply.github.com>
2020-06-23 18:46:31 +0500

    added a child componet named menue

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './App.css';
+import Menue from './menue.js'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <hr/>
+      <h1>food lovers list</h1>
+    <h2>List from parent </h2>
+    <h2>Order your food </h2>
+    <hr/>
+      <b>
+            <li>Biryani</li>
+            <li>Chicken</li>
+            <li>MUtton</li>
+            <li>Pizza</li>
+            </b>
+           <hr/> 
+
+    <Menue d1="biryani" d2="Chicken Karahi" d3="mutton" d4="Pizza"
+    
+    />
+    
+
     </div>
   );
 }
